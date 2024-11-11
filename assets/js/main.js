8560f189c98a385dc84b258b6dfa948b4a4bb135
amdymstfa <amdymoustapha011@gmail.com>
2024-11-11 22:41:50 +0100

    done : caroussel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+// navbar
 const menuButton = document.getElementById('menuButton');
 const mobileMenu = document.getElementById('mobileMenu');
 const menuIcon = document.getElementById('menuIcon');
@@ -10,3 +12,51 @@ menuButton.addEventListener('click', () => {
         menuIcon.innerHTML = `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"/>`;
     }
 });
+// caroussel
+(function () {
+    "use stict"
+    const slideTimeout = 5000;    
+    const $slides = document.querySelectorAll('.slide');
+    let $dots;
+    let intervalId;
+    let currentSlide = 1;
+    function slideTo(index) {
+        currentSlide = index >= $slides.length || index < 1 ? 0 : index;
+        $slides.forEach($elt => $elt.style.transform = `translateX(-${currentSlide * 100}%)`);
+        $dots.forEach(($elt, key) => $elt.classList = `dot ${key === currentSlide? 'active': 'inactive'}`);
+    }
+    function showSlide() {
+        slideTo(currentSlide);
+        currentSlide++;
+    }
+    for (let i = 1; i <= $slides.length; i++) {
+        let dotClass = i == currentSlide ? 'active' : 'inactive';
+        let $dot = `<span data-slidId="${i}" class="dot ${dotClass}"></span>`;
+        document.querySelector('.carousel-dots').innerHTML += $dot;
+    }
+    $dots = document.querySelectorAll('.dot');
+    $dots.forEach(($elt, key) => $elt.addEventListener('click', () => slideTo(key)));
+    
+    intervalId = setInterval(showSlide, slideTimeout)
+    $slides.forEach($elt => {
+        let startX;
+        let endX;
+        $elt.addEventListener('mouseover', () => {
+            clearInterval(intervalId);
+        }, false)
+        $elt.addEventListener('mouseout', () => {
+            intervalId = setInterval(showSlide, slideTimeout);
+        }, false);
+        $elt.addEventListener('touchstart', (event) => {
+            startX = event.touches[0].clientX;
+        });
+        $elt.addEventListener('touchend', (event) => {
+            endX = event.changedTouches[0].clientX;
+            if (startX > endX) {
+                slideTo(currentSlide + 1);
+            } else if (startX < endX) {
+                slideTo(currentSlide - 1);
+            }
+        });
+    })
+})()
