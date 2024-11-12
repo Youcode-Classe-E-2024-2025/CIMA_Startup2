2ea6ef4934e95fbe206e4d87290fa0c4750eec5a
Latifi1937 <abdelalilatifi3@gmail.com>
2024-11-12 13:07:17 +0100

    la réalisation de fonction d'affichage

--- assets/js/All_products.js
+++ assets/js/All_products.js
@@ -1,12 +1,26 @@
-const numbre_elements_page = 16 ;
-let currentpage = 1 ;
+let products = [];  
+let currentpage = 1;
+const numbre_elements_page = 16; 
 
-function displayproducts(page){
-  
-  const startindex = (page - 1)*numbre_elements_page ;
-  const endindex = page + numbre_elements_page ;
-  const currentproducte = product.slice(startindex,endindex) ;
+function displayProducts(page) {
+  const startindex = (page - 1) * numbre_elements_page;
+  const endindex = startindex + numbre_elements_page;
+  const currentProducts = products.slice(startindex, endindex);
+
+  const product_container = document.getElementById("product_container");
+  product_container.innerHTML = ""; 
+
+  currentProducts.forEach(product => {
+    const productdiv = document.createElement("div");
+    productdiv.classList.add("product");
+    productdiv.innerHTML = `
+      <img src="${product.images[0]}" alt="${product.name}" class="w-full h-[80%] object-cover">
+      <div class="mx-2">${product.name}</div>
+      <div class="ml-2 text-darkGolden text-xl">${product.price} $</div>
+    `;
+    
+    product_container.appendChild(productdiv);
+  });
 }
 
-const product_container = document.getElementById("product_container");
-product_container.innerHTML = " "
+
