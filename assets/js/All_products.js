1bf96e50430ef2ce559f6e017b30db2fd2ac8b8c
Latifi1937 <abdelalilatifi3@gmail.com>
2024-11-12 10:29:16 +0100

    vide la page des prouduit

--- assets/js/All_products.js
+++ assets/js/All_products.js
@@ -1,3 +1,12 @@
 const numbre_elements_page = 16 ;
 let currentpage = 1 ;
 
+function displayproducts(page){
+  
+  const startindex = (page - 1)*numbre_elements_page ;
+  const endindex = page + numbre_elements_page ;
+  const currentproducte = product.slice(startindex,endindex) ;
+}
+
+const product_container = document.getElementById("product_container");
+product_container.innerHTML = " "
